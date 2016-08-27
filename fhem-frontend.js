35fafbca6e77cb57646591c4fab0fc9cb24cd04e
Bernd Bestel <bernd@berrnd.de>
2016-08-27 13:02:15 +0200

    Updated fhem-tablet-ui and changes/clean up things for that

--- fhem-frontend.js
+++ fhem-frontend.js
@@ -1,12 +1,4 @@
 ﻿$(function ()
 {
-	$(".timestamp-moment").each(function ()
-	{
-		$(this).on("change", function ()
-		{
-			var element = $(this);
-			var timestamp = element.innerText;
-			element.text(moment(timestamp).fromNow());
-		});
-	});
+	//Nothing currently...
 });
